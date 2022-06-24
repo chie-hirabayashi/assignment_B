87061f8ca17e5908c9425d508b9e36ba4bb4ba91
“chie-hirabayashi” <nyantoroman@yahoo.co.jp>
2022-06-24 17:49:55 +0900

    課題B(staticetics修正)

--- staticetics.py
+++ staticetics.py
@@ -1,44 +1,46 @@
-from __future__ import annotations
-import staticetics
-from audioop import maxpp
-import statistics
-
 input_numbers = input("データを入力してください(スペース区切り) > ")
-
 
 l_si_numbers = f"{input_numbers}".split(" ")
 l_in_numbers = [int(s) for s in l_si_numbers]
-print("組み込み関数")
-print(f"合計値: {sum(l_in_numbers)}")
-print(f"最大値: {max(l_in_numbers)}")
-print(f"最小値: {min(l_in_numbers)}")
-print(f"平均値: {statistics.mean(l_in_numbers)}")
-
-# 組み込み関数つかっちゃダメよー
-print(sep="")
-print("基本統計量の計算")
-
-sum = 0
-for num in l_in_numbers:
-    sum += num
-
-print(f"合計値: {sum}")
-
-max = l_in_numbers[0]
-for num in range(0, len(l_in_numbers)):
-    if l_in_numbers[num] > max:
-        max = l_in_numbers[num]
-
-print(f"最大値: {max}")
-
-min = l_in_numbers[0]
-for num in range(0, len(l_in_numbers)):
-    if l_in_numbers[num] < min:
-        min = l_in_numbers[num]
-
-print(f"最小値: {min}")
 
 
-mean = int(sum / len(l_in_numbers))
+def sum():
+    sum_num = 0
+    for num in l_in_numbers:
+        sum_num += num
+    return sum_num
 
-print(f"平均値: {mean}")
+
+print(f"合計値:{sum()}")
+
+
+def max():
+    max_num = l_in_numbers[0]
+    for num in range(0, len(l_in_numbers)):
+        if l_in_numbers[num] > int(max_num):
+            max_num = l_in_numbers[num]
+    return max_num
+
+
+print(f"最大値:{max()}")
+
+
+def min():
+    min_num = l_in_numbers[0]
+    for num in range(0, len(l_in_numbers)):
+        if l_in_numbers[num] < int(min_num):
+            min_num = l_in_numbers[num]
+    return min_num
+
+
+print(f"最小値:{min()}")
+
+
+def mean():
+    sum_num = 0
+    for num in l_in_numbers:
+        sum_num += num
+    return sum_num / len(l_in_numbers)
+
+
+print(f"平均値:{mean()}")
